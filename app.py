6f26851f7956eb1571bc604e695b21fd5ad4e3de
NikaBatsmanova <n.shevcsova@gmail.com>
2021-01-28 14:38:11 +0300

    changed method handling

--- app.py
+++ app.py
@@ -1,54 +1,54 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-import random
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
-api = Api(app)
+app.config["DEBUG"] = True
+
 ai_menu = [
     {
-        "id": 0,
+        "id": 1,
         "day of week": "Monday",
         "breakfast": ["tea", "oatmeal with butter"],
         "dinner": ["jelly", "cabbage soup", "mashed potatoes with a chop"],
         "lunch": ["tea", "ragout"]
     },
     {
-        "id": 1,
+        "id": 2,
         "day of week": "Tuesday",
         "breakfast": ["cocoa", "sandwiches"],
         "dinner": ["compote", "borscht", "braised cabbage"],
         "lunch": ["tea", "french meat"]
     },
     {
-        "id": 2,
+        "id": 3,
         "day of week": "Wednesday",
         "breakfast": ["orange juice", "omelet"],
         "dinner": ["jelly", "pea soup", "pasta with chicken fillet"],
         "lunch": ["tea", "mac and cheese"]
     },
     {
-        "id": 3,
+        "id": 4,
         "day of week": "Thursday",
         "breakfast": ["tea", "buckwheat porridge with sugar"],
         "dinner": ["jelly", "hodgepodge soup", "pilaf"],
         "lunch": ["fruit drink", "pizza"]
     },
     {
-        "id": 4,
+        "id": 5,
         "day of week": "Friday",
         "breakfast": ["cocoa", "cottage cheese casserole"],
         "dinner": ["compote", "meatball soup", "vitamin salad"],
         "lunch": ["tea", "potato casserole"]
     },
     {
-        "id": 5,
+        "id": 6,
         "day of week": "Saturday",
         "breakfast": ["tea", "porridge friendship"],
         "dinner": ["jelly", "milk soup", "buckwheat with sausages"],
         "lunch": ["cherry juice", "steamed vegetables"]
     },
     {
-        "id": 6,
+        "id": 7,
         "day of week": "Sunday",
         "breakfast": ["tea", "corn porridge"],
         "dinner": ["jelly", "potato soup", "meat with vegetables"],
@@ -57,67 +57,77 @@ ai_menu = [
 ]
 
 
-class Menu(Resource):
-    def get(self, id=0):
-        if id == 0:
-            return random.choice(ai_menu), 200
-        for menu in ai_menu:
-            if (menu["id"] == id):
-                return menu, 200
-        return "Menu not found", 404
-
-    def post(self, id):
-        parser = reqparse.RequestParser()
-        parser.add_argument("day of week")
-        parser.add_argument("breakfast")
-        parser.add_argument("dinner")
-        parser.add_argument("lunch")
-        params = parser.parse_args()
-        for menu in ai_menu:
-            if (id == menu["id"]):
-                return f"Menu with id {id} already exists", 400
-        menu = {
-            "id": int(id),
-            "day of week": params["day of week"],
-            "breakfast": params["breakfast"],
-            "dinner": params["dinner"],
-            "lunch": params["lunch"]
-        }
-        ai_menu.append(menu)
-        return menu, 201
-
-    def put(self, id):
-        parser = reqparse.RequestParser()
-        parser.add_argument("day of week")
-        parser.add_argument("breakfast")
-        parser.add_argument("dinner")
-        parser.add_argument("lunch")
-        params = parser.parse_args()
-        for menu in ai_menu:
-            if (id == menu["id"]):
-                menu["day of week"] = params["day of week"]
-                menu["breakfast"] = params["breakfast"]
-                menu["dinner"] = params["dinner"]
-                menu["lunch"] = params["lunch"]
-                return menu, 200
-
-        menu = {
-            "id": int(id),
-            "day of week": params["day of week"],
-            "breakfast": params["breakfast"],
-            "dinner": params["dinner"],
-            "lunch": params["lunch"]
-        }
-
-        ai_menu.append(menu)
-        return menu, 201
-
-    def delete(self, id):
-        global ai_menu
-        ai_menu = [menu for menu in ai_menu if menu["id"] != id]
-        return f"Menu with id {id} is deleted.", 200
+@app.route('/menu/all', methods=['GET'])
+def api_all():
+    return jsonify(ai_menu)
 
 
-api.add_resource(Menu, "/ai-menu", "/ai-menu/", "/ai-menu/", "/ai-menu/", "/ai-menu/<int:id>")
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/menu', methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+    results = []
+    for menu in ai_menu:
+        if (menu["id"] == id):
+            results.append(menu)
+    return jsonify(results)
+
+
+@app.route('/menu', methods=['POST'])
+def api_post():
+    id = int(request.form['id'])
+    day = request.form['day']
+    breakfast = request.form['breakfast']
+    lunch = request.form['lunch']
+    dinner = request.form['dinner']
+    for menu in ai_menu:
+        if (id == menu["id"]):
+            return f"Menu with id {id} already exists", 400
+    menu = {
+        "id": int(id),
+        "day of week": day,
+        "breakfast": breakfast,
+        "dinner": lunch,
+        "lunch": dinner
+    }
+    ai_menu.append(menu)
+    return jsonify(ai_menu)
+
+
+@app.route('/menu', methods=['PUT'])
+def put():
+    id = int(request.form['id'])
+    day = request.form['day']
+    breakfast = request.form['breakfast']
+    lunch = request.form['lunch']
+    dinner = request.form['dinner']
+    for menu in ai_menu:
+        if (id == menu["id"]):
+            menu["day of week"] = day
+            menu["breakfast"] = breakfast
+            menu["dinner"] = lunch
+            menu["lunch"] = dinner
+            return jsonify(ai_menu)
+
+    menu = {
+        "id": int(id),
+        "day of week": day,
+        "breakfast": breakfast,
+        "dinner": lunch,
+        "lunch": dinner
+    }
+    ai_menu.append(menu)
+    return jsonify(ai_menu)
+
+
+@app.route('/menu', methods=['DELETE'])
+def delete():
+    id = int(request.form['id'])
+    global ai_menu
+    ai_menu = [menu for menu in ai_menu if menu["id"] != id]
+    return jsonify(ai_menu)
+
+
+app.run()
